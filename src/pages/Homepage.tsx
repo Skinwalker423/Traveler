4cce5ed03a89df021d9a49403921110a115c1398
Luis Gonzalez <lgonzalez23@verizon.net>
2024-03-08 17:04:32 -0800

    pages starter

--- src/pages/Homepage.tsx
+++ src/pages/Homepage.tsx
@@ -1,10 +1,12 @@
 import { Link } from "react-router-dom";
 import { PagNav } from "../components/PagNav";
+import AppNav from "../components/AppNav";
 
 const Homepage = () => {
   return (
     <div>
       <PagNav />
+      <AppNav />
       <h1>Traveler</h1>
       <Link to={"/app"}>Go to App</Link>
     </div>
