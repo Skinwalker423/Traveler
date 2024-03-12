c4d7e4cafd578c739795189106a3df0d49420cc2
Luis Gonzalez <lgonzalez23@verizon.net>
2024-03-12 16:00:53 -0700

    app layout map

--- src/pages/AppLayout.tsx
+++ src/pages/AppLayout.tsx
@@ -1,10 +1,12 @@
 import style from "./AppLayout.module.css";
 import Sidebar from "../components/Sidebar";
+import Map from "../components/Map";
 
 export default function AppLayout() {
   return (
     <div className={style.app}>
       <Sidebar />
+      <Map />
     </div>
   );
 }
