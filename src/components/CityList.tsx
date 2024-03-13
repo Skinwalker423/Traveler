4974261643b0efbe621d4e2465ca206a617c6aeb
Luis Gonzalez <lgonzalez23@verizon.net>
2024-03-13 16:45:54 -0700

    mock server city data

--- src/components/CityList.tsx
+++ src/components/CityList.tsx
@@ -1,5 +1,33 @@
+import { useEffect, useState } from "react";
+import styles from "./CityList.module.css";
+
 const CityList = () => {
-  return <div>CityList</div>;
+  const [cities, setCities] = useState<any>([]);
+
+  const fetchCities = async () => {
+    const response = await fetch(
+      "http://localhost:3000/cities"
+    );
+    const citiesData = await response.json();
+    if (citiesData.length) {
+      setCities(citiesData);
+    }
+    console.log("response", response);
+  };
+
+  useEffect(() => {
+    fetchCities();
+  }, []);
+
+  if (!cities.length) return null;
+
+  const renderCities = cities.map((city) => {
+    return <p key={city.id}>{city.cityName}</p>;
+  });
+
+  return (
+    <ul className={styles.cityList}>{renderCities}</ul>
+  );
 };
 
 export default CityList;
