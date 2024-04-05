b1944c6f305da2fd6f8707a0c5af04eed6eaca68
Skinwalker423 <85347852+Skinwalker423@users.noreply.github.com>
2024-04-04 17:19:49 -0700

    country list

--- src/components/CountryList.tsx
+++ src/components/CountryList.tsx
@@ -1,5 +1,57 @@
+import { useContext } from "react";
+
+import { LocationContext } from "../context/LocationContext";
+import Spinner from "./Spinner";
+import Message from "./Message";
+
+import styles from "./CountryList.module.css";
+import CountryItem from "./CountryItem";
+
 const CountryList = () => {
-  return <div>CountryList</div>;
+  const { cities, isLoading } = useContext(LocationContext);
+
+  if (isLoading) return <Spinner />;
+
+  if (!cities.length)
+    return (
+      <Message message='Add your first city by clicking on a city on the map' />
+    );
+
+  let countries: {
+    id: string;
+    emoji: string;
+    country: string;
+  }[] = [];
+
+  cities.forEach((city) => {
+    if (
+      !countries.find(
+        (country) => country.country === city.country
+      )
+    ) {
+      countries = [
+        ...countries,
+        {
+          id: city.country,
+          country: city.country,
+          emoji: city.emoji,
+        },
+      ];
+    }
+  });
+
+  return (
+    <ul className={styles.countryList}>
+      {countries.map((country) => {
+        return (
+          <CountryItem
+            key={country.country}
+            country={country}
+          />
+        );
+      })}
+    </ul>
+  );
 };
 
 export default CountryList;
