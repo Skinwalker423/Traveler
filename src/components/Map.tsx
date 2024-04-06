84f0709b074e2ec58eccb72f62ac4328a5c6d399
Skinwalker423 <85347852+Skinwalker423@users.noreply.github.com>
2024-04-05 17:33:57 -0700

    use search params

--- src/components/Map.tsx
+++ src/components/Map.tsx
@@ -1,6 +1,29 @@
+import { useSearchParams } from "react-router-dom";
 import styles from "./Map.module.css";
+
 const Map = () => {
-  return <div className={styles.mapContainer}>Map</div>;
+  const [searchParams, setSearcghParams] =
+    useSearchParams();
+  const lat = searchParams.get("lat");
+  const lng = searchParams.get("lng");
+
+  const updateUrl = (lat: string, lng: string) => {
+    setSearcghParams({
+      lat,
+      lng,
+    });
+  };
+
+  return (
+    <div className={styles.mapContainer}>
+      <h2>Map Coordinates:</h2>
+      <p>Lat: {lat}</p>
+      <p>Long: {lng}</p>
+      <button onClick={() => updateUrl("25.5", "20")}>
+        Update
+      </button>
+    </div>
+  );
 };
 
 export default Map;
