3a5b7b7b82626cbae270a3f630fd3dba81d20a70
Skinwalker423 <85347852+Skinwalker423@users.noreply.github.com>
2024-04-10 17:52:00 -0700

    create city

--- src/actions/index.ts
+++ src/actions/index.ts
@@ -6,7 +6,28 @@ export const fetchCities = async () => {
       "http://localhost:3000/cities"
     );
     const citiesData = await response.json();
-    return citiesData;
+    return citiesData as City[];
+  } catch (error: any) {
+    console.error(error.message);
+  }
+};
+
+export const createCity = async (newCity: City) => {
+  if (!newCity) return;
+  try {
+    const response = await fetch(
+      "http://localhost:3000/cities",
+      {
+        body: JSON.stringify(newCity),
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+      }
+    );
+    const data = await response.json();
+    console.log("data after create city", data);
+    return data;
   } catch (error: any) {
     console.error(error.message);
   }
