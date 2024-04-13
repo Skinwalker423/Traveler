6e35f1480ff944fe1bbcb6ba54b16e039aca407a
Skinwalker423 <85347852+Skinwalker423@users.noreply.github.com>
2024-04-12 18:24:14 -0700

    reducer

--- src/context/LocationContext.tsx
+++ src/context/LocationContext.tsx
@@ -2,6 +2,7 @@ import {
   ReactNode,
   createContext,
   useEffect,
+  useReducer,
   useState,
 } from "react";
 import { City } from "../types";
@@ -19,16 +20,60 @@ export const LocationContext = createContext(
   {} as LocationContextType
 );
 
+interface StateProps {
+  cities: City[];
+  isLoading: boolean;
+  currentCity: City | {};
+}
+
+const initialState: StateProps = {
+  cities: [],
+  isLoading: false,
+  currentCity: {},
+};
+
+function reducer(state: StateProps, action) {
+  switch (action.type) {
+    case "cities/loaded":
+      return {
+        ...state,
+        isLoading: false,
+        cities: action.payload,
+      };
+    case "cities/created":
+      return {
+        ...state,
+        cities: [...state.cities, action.payload],
+      };
+    case "cities/deleted":
+      const filteredList = state.cities.filter(
+        (city) => city.id !== action.payload
+      );
+      return {
+        ...state,
+        cities: filteredList,
+      };
+
+    default:
+      throw new Error("unknown action type");
+  }
+}
+
 export const LocationProvider = ({
   children,
 }: {
   children: ReactNode;
 }) => {
-  const [cities, setCities] = useState<City[]>([]);
-  const [currentCity, setCurrentCity] = useState<
-    City | undefined
-  >();
-  const [isLoading, setIsLoading] = useState(false);
+  const [state, dispatch] = useReducer(
+    reducer,
+    initialState
+  );
+
+  // const [cities, setCities] = useState<City[]>([]);
+  // const [currentCity, setCurrentCity] = useState<
+  //   City | undefined
+  // >();
+  // const [isLoading, setIsLoading] = useState(false);
 
   const addCityToList = (city: City) => {
     setCities((cities) => [...cities, city]);
