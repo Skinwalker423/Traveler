05bbfe40633568ab4f1153ec7029fa87e87fbb17
Skinwalker423 <85347852+Skinwalker423@users.noreply.github.com>
2024-04-15 16:44:10 -0700

    hero action button redirect to login

--- src/pages/Homepage.tsx
+++ src/pages/Homepage.tsx
@@ -1,8 +1,10 @@
 import { Link } from "react-router-dom";
 import styles from "./Homepage.module.css";
 import { PagNav } from "../components/PagNav";
+import useAuth from "../hooks/useAuth";
 
 export default function Homepage() {
+  const { state } = useAuth();
   return (
     <main className={styles.homepage}>
       <PagNav />
@@ -18,7 +20,10 @@ export default function Homepage() {
           experiences, and show your friends how you have
           wandered the world.
         </h2>
-        <Link to={"/app"} className='cta'>
+        <Link
+          to={state.isAuthenticated ? "/app" : "/login"}
+          className='cta'
+        >
           Start trecking now
         </Link>
       </section>
